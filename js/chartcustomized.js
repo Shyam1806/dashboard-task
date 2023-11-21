07312b25809c98a2e3f9246569a6a0b0712a93c6
Shyam1806 <62296718+Shyam1806@users.noreply.github.com>
2023-11-21 14:03:42 +0530

    "chart updated"

--- js/chartcustomized.js
+++ js/chartcustomized.js
@@ -21,7 +21,7 @@ $(function () {
       },
   
   
-      colors: ["#5D87FF", "#49BEFF"],
+      colors: ["#b394f1", "#49BEFF"],
   
   
       plotOptions: {
@@ -364,62 +364,114 @@ var optionstarget = {
   chart.render();
 
   // profit chart
-  var profitoptions = {
+  // var profitoptions = {
+  //   series: [{
+  //   name: '$256.48',
+  //   width: 280,
+  //   data: [44, 55, 57, 56, 61, 58, 63],
+  //   colors:"#000"
+  // }, {
+  //   name: '$187.39',
+  //   data: [76, 85, 101, 98, 87, 105, 91]
+  // }, ],
+  //   chart: {
+  //   type: 'bar',
+  //   height: 200
+  // },
+  // plotOptions: {
+  //   bar: {
+  //     horizontal: false,
+  //     columnWidth: '55%',
+  //     endingShape: 'rounded'
+  //   },
+  // },
+  // dataLabels: {
+  //   enabled: false
+  // },
+  // stroke: {
+  //   show: true,
+  //   width: 2,
+  //   colors: ['transparent']
+  // },
+  // xaxis: {
+  //   categories: ['SUN' , 'MON' , 'TUE', 'WED' , 'THU' , 'FRI' , 'SAT'],
+  // },
+  // yaxis: {
+    
+  //   labels:{
+  //     style:{
+  //       colors: "#000"
+  //     }
+  //   },
+  // },
+  
+  // fill: {
+  //   opacity: 1
+  // },
+ 
+  // tooltip: {
+  //   y: {
+  //     formatter: function (val) {
+  //       return "$ " + val + ".."
+  //     }
+  //   }
+  // }
+  // };
+  var profitoptions =options = {
     series: [{
-    name: '$256.48',
-    width: 280,
-    data: [44, 55, 57, 56, 61, 58, 63],
-    colors:"#E96559"
-  }, {
-    name: '$187.39',
-    data: [76, 85, 101, 98, 87, 105, 91]
-  }, ],
+    name: 'Sales',
+    data: [10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 18]
+  }],
     chart: {
-    type: 'bar',
-    height: 200
+    height: 200,
+    type: 'line',
   },
-  plotOptions: {
-    bar: {
-      horizontal: false,
-      columnWidth: '55%',
-      endingShape: 'rounded'
-    },
-  },
-  dataLabels: {
-    enabled: false
+  forecastDataPoints: {
+    count: 7
   },
   stroke: {
-    show: true,
-    width: 2,
-    colors: ['transparent']
+    width: 5,
+    curve: 'smooth'
   },
   xaxis: {
-    categories: ['SUN' , 'MON' , 'TUE', 'WED' , 'THU' , 'FRI' , 'SAT'],
-  },
-  yaxis: {
-    
-    labels:{
-      style:{
-        colors: "#FF1654"
-      }
-    },
-  },
-  
-  fill: {
-    opacity: 1
-  },
- 
-  tooltip: {
-    y: {
-      formatter: function (val) {
-        return "$ " + val + " thousands"
+    type: 'datetime',
+    categories: [ '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000', '7/11/2000', '8/11/2000', '9/11/2000', '10/11/2000', '11/11/2000', '12/11/2000', '1/11/2001', '2/11/2001', '3/11/2001','4/11/2001' ,'5/11/2001' ,'6/11/2001'],
+    tickAmount: 10,
+    labels: {
+      formatter: function(value, timestamp, opts) {
+        return opts.dateFormatter(new Date(timestamp), 'dd MMM')
       }
     }
+  },
+  // title: {
+  //   text: 'Forecast',
+  //   align: 'left',
+  //   style: {
+  //     fontSize: "16px",
+  //     color: '#666'
+  //   }
+  // },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      gradientToColors: [ '#FDD835'],
+      shadeIntensity: 1,
+      type: 'horizontal',
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [0, 100, 100, 100]
+    },
+  },
+  yaxis: {
+    min: -10,
+    max: 40
   }
   };
+  new ApexCharts(document.querySelector("#profitchart"), profitoptions).render();
 
-  var chart = new ApexCharts(document.querySelector("#profitchart"), profitoptions);
-  chart.render();
+  // var chart = new ApexCharts(document.querySelector("#profitchart"), profitoptions);
+  // chart.render();
 // polar chart
 var options = {
     series: [44, 55, 13, 33],
